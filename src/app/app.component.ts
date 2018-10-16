08d7b127ea55ecb7e1d9d13c1a27da52fb317145
https://github.com/MNIFM/BlogApp <mahdi.mnif1@gmail.com>
2018-10-16 17:08:10 +0200

    16/10/2018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { _createNgProbe } from '@angular/platform-browser/src/dom/debug/ng_probe';
+import { Observable, Subscription } from 'rxjs/Rx';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProxEvent';
+export class AppComponent implements OnInit, OnDestroy  {
+
+  secondes: number;
+  counterSubscription: Subscription;
+  constructor() {
+  }
+ ngOnInit () {
+  const counter = Observable.interval(1000);
+  this.counterSubscription = counter.subscribe(
+    (value: number) => {
+      this.secondes = value;
+    }
+  );
+  }
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AddEventComponent } from './events/add-event/add-event.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+const appRoutes: Routes = [
+
+  { path: 'accueil', canActivate: [AuthGuard], component: AccueilComponent },
+  { path: 'addevent', canActivate: [AuthGuard] , component: AddEventComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AuthComponent },
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEventComponent,
+    AccueilComponent,
+    AuthComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+     AuthService,
+     AuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
